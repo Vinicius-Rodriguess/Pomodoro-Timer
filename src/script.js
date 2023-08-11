d723a3efe645079f1706f5543bc9ac695e197ab6
Vinicius-Rodriguess <123024036+Vinicius-Rodriguess@users.noreply.github.com>
2023-08-11 17:54:27 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -38,7 +38,7 @@ document.addEventListener('click', (e) => {
             return;
         }
         
-        timer = setInterval(contagemRegressiva, 1000);
+        timer = setInterval(contagemRegressiva, 10);
     }
 
     if (e.target.classList.contains('stop')) {
